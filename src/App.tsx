f3a53bc7b74f6a0c64b1bf99519829aa93aaa409
damadu01 <damadu01@gmail.com>
2026-01-16 08:10:22 +0000

    2단계 과제: 1단계에서는 정적인 텍스트를 렌더링했습니다. 이제 동적인 데이터를 JSX에 주입하는 방법을 배워봅시다. App 컴포넌트 내부에 user라는 객체를 만들고, 그 객체의 속성을 JSX 안에서 동적으로 출력해보세요.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import './App.css'
 
+
 function App() {
+
+  let user = {
+    name: "John",
+    age: 30
+  }
+
   return (
-    <h1>Hello, World!</h1>
+    <>
+      <h1>안녕하세요, {user.name}님! 10년 뒤 당신은 {user.age + 10}살입니다.</h1>
+    </>
   )
 }
 
