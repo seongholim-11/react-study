3eff59606854428e779fff1870f28a1e55ae99e0
damadu01 <damadu01@gmail.com>
2026-01-16 12:54:58 +0000

    7단계 과제: 지금까지 배운 useState (상태 관리), map (리스트 렌더링), input (폼 핸들링)을 모두 사용하여 간단한 '할 일 목록(To-Do List)' 애플리케이션을 만들어 보세요.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,53 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
+
+// 타입/인터페이스 이름은 대문자로 시작하는 것이 컨벤션입니다. (todo -> Todo)
+interface Todo {
+  id: number;
+  text: string;
+}
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
+  // useState에 제네릭으로 타입을 명시해줍니다.
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  function handleAddTodo() {
+    // 아무것도 입력 안했을 때 추가되지 않도록 방어 로직 추가 (선택사항이었지만 좋은 습관입니다)
+    if (newTodo.trim() === '') {
+      return; 
+    }
+
+    const todoToAdd: Todo = {
+      id: Date.now(),
+      text: newTodo,
+    };
+
+    // 불변성을 지키며 새로운 배열로 상태 업데이트
+    setTodos(prevTodos => [...prevTodos, todoToAdd]);
+    setNewTodo(''); // 입력창 비우기
+  }
 
   return (
     <div>
-      <input type="text" value={inputValue} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value)} />
-      <p>입력된 값: [{inputValue}]</p>
+      <h1>My To-Do List</h1>
+      <div>
+        <input 
+          type="text" 
+          value={newTodo} 
+          onChange={(e) => setNewTodo(e.target.value)} 
+          placeholder="할 일을 입력하세요"
+        />
+        <button onClick={handleAddTodo}>추가</button>
+      </div>
+
+      {/* 할 일 목록을 렌더링하는 부분 */}
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
