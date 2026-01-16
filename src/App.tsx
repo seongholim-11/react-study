4e5855652298d511e677732011a213a4fc386eb1
damadu01 <damadu01@gmail.com>
2026-01-16 08:27:25 +0000

    3단계 과제: 지금까지는 모든 코드를 App 컴포넌트 하나에 작성했습니다. 실무에서는 기능별로 컴포넌트를 분리하여 재사용성을 높입니다. 2단계에서 만들었던 사용자 정보 출력 부분을 별도의 UserInfo 컴포넌트로 분리하고, App 컴포넌트에서 UserInfo 컴포넌트로 데이터를 전달해보세요.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import './App.css'
-
+import UserInfo from './UserInfo'
 
 function App() {
 
-  let user = {
+  const user = {
     name: "John",
     age: 30
   }
 
   return (
-    <>
-      <h1>안녕하세요, {user.name}님! 10년 뒤 당신은 {user.age + 10}살입니다.</h1>
-    </>
+    <UserInfo user={user} />
   )
 }
 
---
+++ src/UserInfo.tsx
@@ -0,0 +1,14 @@
+interface UserInfoProps {
+    user: {
+        name: string;
+        age: number;
+    }
+};
+
+const UserInfo: React.FC<UserInfoProps> = ({ user }) => {
+    return (
+        <h2>안녕하세요, {user.name}님! 10년 뒤 당신은 {user.age + 10}살입니다.</h2>
+    )
+}
+
+export default UserInfo
